04d18952e0ba94c4d68c573ebfa5d0d74aa40821
Frederic Vogels <frederic.vogels@ucll.be>
2021-04-14 11:36:53 +0200

    added comments to tests.cpp

--- src/huffman/huffman/tests/tests.cpp
+++ src/huffman/huffman/tests/tests.cpp
@@ -1,4 +1,11 @@
 #ifdef TEST_BUILD
+/*
+* This file is only active in testing build.
+* Its purpose is to initialize the testing framework.
+* 
+* You can safely ignore this file; no changes will have to be made to it.
+* Make sure this file remains included in the project though: without it, tests will not be able to run.
+*/
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
